868c509b346324742e76ddc99eb82a3bff43e55e
shavin-spec <sharikav2005@gmail.com>
2025-04-05 00:06:42 +0530

    feat: recognize english text

--- App.js
+++ App.js
@@ -2,16 +2,45 @@ import React, { useEffect, useState } from "react";
 import { View, Button, Alert, Image, Text } from "react-native";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { launchImageLibrary, launchCamera } from "react-native-image-picker";
+import { NativeModules } from 'react-native';
+
+const { TextRecognitionModule } = NativeModules;
 
 const App = () => {
   const [user, setUser] = useState(null);
   const [image, setImage] = useState(null); // State to store image URI
+
+  const [response, setResposne] = useState();
+  const [resultText, setResultText] = useState("");
+
 
   useEffect(() => {
     GoogleSignin.configure({
       iosClientId: "710398868233-jh26kvmcsqtm8jgoiflagvfj7om44itc.apps.googleusercontent.com", // Replace with your iOS client ID
     });
   }, []);
+
+  const onPress = async () => {
+    if (image) {
+      console.log('We will invoke the native module here!');
+      const response = await TextRecognitionModule.recognizeImage(image);
+      console.log(response, "res")
+      const extractText = (resp) => {
+        if (!resp || !resp.blocks) return "";
+        return resp.blocks
+          .map((block) => block?.text)
+          .filter(Boolean)
+          .join("\n");
+      };
+
+      const text = extractText(response);
+      setResultText(text); // Save to state
+      console.log(text, "resultText::::::::");
+    } else {
+      Alert.alert("Error", "Please select an image first");
+    }
+  };
+
 
   const signInWithGoogle = async () => {
     try {
@@ -75,6 +104,18 @@ const App = () => {
           <Button title="Pick Image from Gallery" onPress={pickImage} />
           <Button title="Take a Photo" onPress={takePhoto} />
           <Button title="Sign Out" onPress={signOut} color="red" />
+          {image && (
+            <Button
+              title="Recognize Text"
+              color="#841584"
+              onPress={onPress}
+            />
+          )}
+          {resultText !== "" && (
+            <View style={{ padding: 10 }}>
+              <Text style={{ fontSize: 16, color: "#333" }}>{resultText}</Text>
+            </View>
+          )}
         </>
       ) : (
         <Button title="Sign in with Google" onPress={signInWithGoogle} />
---
+++ ios/MLKit/RCTTextRecognitionModule.h
@@ -0,0 +1,18 @@
+//
+//  RCTTextRecognitionModule.h
+//  PANCardReader
+//
+//  Created by Chitra on 04/04/25.
+//
+
+#ifndef RCTTextRecognitionModule_h
+#define RCTTextRecognitionModule_h
+
+
+//  RCTCalendarModule.h
+#import <React/RCTBridgeModule.h>
+@interface RCTTextRecognitionModule : NSObject <RCTBridgeModule>
+@end
+
+
+#endif /* RCTTextRecognitionModule_h */
