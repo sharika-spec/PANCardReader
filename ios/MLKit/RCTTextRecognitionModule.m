be9e58a51d08d9910fb17965c3ea046f900f5718
shavin-spec <sharikav2005@gmail.com>
2025-04-08 18:25:54 +0530

    chore: remove dimensions

--- ios/MLKit/RCTTextRecognitionModule.m
+++ ios/MLKit/RCTTextRecognitionModule.m
@@ -15,16 +15,6 @@
 
 RCT_EXPORT_MODULE(TextRecognitionModule);
 
-- (NSMutableDictionary *)getFrameDictionary:(CGRect)frame {
-  NSMutableDictionary *rect = [NSMutableDictionary dictionary];
-  
-  [rect setValue:[NSNumber numberWithFloat:frame.origin.x] forKey:@"left"];
-  [rect setValue:[NSNumber numberWithFloat:frame.origin.y] forKey:@"top"];
-  [rect setValue:[NSNumber numberWithFloat:frame.size.width] forKey:@"width"];
-  [rect setValue:[NSNumber numberWithFloat:frame.size.height] forKey:@"height"];
-  
-  return rect;
-}
 
 RCT_EXPORT_METHOD(recognizeImage:(NSString *)url
                   resolver:(RCTPromiseResolveBlock)resolve
@@ -37,9 +27,7 @@ RCT_EXPORT_METHOD(recognizeImage:(NSString *)url
   UIImage *image = [UIImage imageWithData:imageData];
 
   MLKVisionImage *visionImage = [[MLKVisionImage alloc] initWithImage:image];
-  
-//  MLKTextRecognizer *textRecognizer = [MLKTextRecognizer textRecognizer];
-  
+    
   // When using Devanagari script recognition SDK
   MLKDevanagariTextRecognizerOptions *devanagariOptions = [[MLKDevanagariTextRecognizerOptions alloc] init];
   MLKTextRecognizer *devanagariTextRecognizer = [MLKTextRecognizer textRecognizerWithOptions:devanagariOptions];
@@ -55,8 +43,6 @@ RCT_EXPORT_METHOD(recognizeImage:(NSString *)url
     
     NSMutableDictionary *response = [NSMutableDictionary dictionary];
     
-    [response setValue:[NSNumber numberWithInt:image.size.width] forKey:@"width"];
-    [response setValue:[NSNumber numberWithInt:image.size.height] forKey:@"height"];
     
     NSMutableArray *blocks = [NSMutableArray array];
     // Recognized text
@@ -64,14 +50,12 @@ RCT_EXPORT_METHOD(recognizeImage:(NSString *)url
     for (MLKTextBlock *block in result.blocks) {
       NSMutableDictionary *blockDict = [NSMutableDictionary dictionary];
       [blockDict setValue:block.text forKey:@"text"];
-      [blockDict setValue:[self getFrameDictionary:block.frame] forKey:@"rect"];
       
       
       NSMutableArray *lines = [NSMutableArray array];
       for (MLKTextLine *line in block.lines) {
         NSMutableDictionary *lineDict = [NSMutableDictionary dictionary];
         [lineDict setValue:line.text forKey:@"text"];
-        [lineDict setValue:[self getFrameDictionary:line.frame] forKey:@"rect"];
        
         [lines addObject:lineDict];
       }
